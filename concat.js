93acbe61ced7ffdf522c011cb797ddbb7e88563e
Aidan Middleton <aidanmiddleton@Aidans-MacBook-Pro.local>
2020-12-15 13:50:06 -0800

    first commit

---
+++ concat.js
@@ -0,0 +1,15 @@
+// Create a function to concatenate two integer arrays. it will take in two arrays, 
+// and return one new one.
+
+
+
+
+
+concat([1, 3, 5], [2, 6, 8]) 
+// ➞ [1, 3, 5, 2, 6, 8]
+
+concat([7, 8], [10, 9, 1, 1, 2]) 
+// ➞ [7, 8, 10, 9, 1, 1, 2]
+
+concat([4, 5, 1], [3, 3, 3, 3, 3]) 
+// ➞ [4, 5, 1, 3, 3, 3, 3, 3]
---
+++ countOnes.js
@@ -0,0 +1,28 @@
+// Create a function to count the number of 1s in a 2D array.
+
+
+
+
+
+countOnes([
+    [1, 0],
+    [0, 0]
+])
+
+// should return 1
+
+countOnes([
+    [1, 1, 1],
+    [0, 0, 1],
+    [1, 1, 1]
+])
+
+// should return 7
+
+countOnes([
+    [1, 2, 3],
+    [0, 2, 1],
+    [5, 7, 33]
+])
+
+//should return 2

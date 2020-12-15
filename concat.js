e6b5850233a3a1fb26e45dc0b9e18abb5b8844b5
Aidan Middleton <aidanmiddleton@Aidans-MacBook-Pro.local>
2020-12-15 14:30:16 -0800

    changes

--- concat.js
+++ concat.js
@@ -1,15 +1,17 @@
-// Create a function to concatenate two integer arrays. it will take in two arrays, 
+// Create a function to concatenate (add) two integer arrays. it will take in two arrays, 
 // and return one new one.
 
+// do not use the .concat() method. 
 
 
+// your code here
 
 
-concat([1, 3, 5], [2, 6, 8]) 
+console.log(concat([1, 3, 5], [2, 6, 8])); 
 // ➞ [1, 3, 5, 2, 6, 8]
 
-concat([7, 8], [10, 9, 1, 1, 2]) 
+console.log(concat([7, 8], [10, 9, 1, 1, 2])); 
 // ➞ [7, 8, 10, 9, 1, 1, 2]
 
-concat([4, 5, 1], [3, 3, 3, 3, 3]) 
+console.log(concat([4, 5, 1], [3, 3, 3, 3, 3])); 
 // ➞ [4, 5, 1, 3, 3, 3, 3, 3]
--- lessThanGreaterThan.js
+++ lessThanGreaterThan.js
@@ -1,16 +1,16 @@
 // Create a function that takes two numbers num1, num2, and an array arr and 
 //returns an array containing all the numbers in arr greater than num1 and less than num2.
 
+//your code here
 
 
 
 
-
-arrBetween(3, 8, [1, 5, 95, 0, 4, 7]) 
+console.log(arrBetween(3, 8, [1, 5, 95, 0, 4, 7])); 
 // ➞ [5, 4, 7]
 
-arrBetween(1, 10, [1, 10, 25, 8, 11, 6]) 
+console.log(arrBetween(1, 10, [1, 10, 25, 8, 11, 6]) );
 // ➞ [8, 6]
 
-arrBetween(7, 32, [1, 2, 3, 78]) 
+console.log(arrBetween(7, 32, [1, 2, 3, 78]) );
 //➞ []
